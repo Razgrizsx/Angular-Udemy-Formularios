237d8190cbc8efc5b03056cacf11879e07eec532
Razgrizsx <razgrisss41@gmail.com>
2023-09-05 20:10:33 -0300

    Formularios Reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
 
+  constructor(private formBuilder : FormBuilder){}
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: "Radeon RX 6600 XT",
+      precio: 200000
+    })
+  }
+
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre:['', [Validators.required, Validators.minLength(3)]],
+    precio:[0, [Validators.required, Validators.min(0)]],
+    existencias:[0, [Validators.required, Validators.min(0)]]
+  })
+
+  campoValido(campo: string){
+   return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+  }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      console.log('Aqui')
+      this.miFormulario.markAllAsTouched()
+      return;
+    }
+    console.log(this.miFormulario.value)
+    this.miFormulario.reset()
+  }
+/* 
+  miFormulario : FormGroup = new FormGroup({
+    nombre: new FormControl('Radeon RX 6600 XT'),
+    precio: new FormControl(200000),
+    existencias: new FormControl(50)
+  }) */
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class DinamicosComponent {
 
+  constructor(private formBuilder : FormBuilder){}
+
+  nuevoFavorito : FormControl = this.formBuilder.control('', [Validators.required])
+
+  miFormulario : FormGroup = this.formBuilder.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.formBuilder.array([["Metal Gear Solid"], ["Final Fantasy"]], Validators.required)
+  })
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray 
+  }
+
+
+  campoValido(campo: string){
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+  }
+
+  agregarFavorito(){
+    if(this.nuevoFavorito.invalid){
+      return
+    }
+    this.favoritosArr.push( this.formBuilder.control(this.nuevoFavorito.value, Validators.required) )
+    this.nuevoFavorito.reset()
+  }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+    console.log(this.miFormulario.value)
+    this.miFormulario.reset()
+  }
 }
